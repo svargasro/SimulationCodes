9ff1a97639159bc287c4036f853311cf499b459f
svargasro <svargasro@unal.edu.co>
2024-09-04 00:33:40 -0500

    No spaces

--- proyectoFinal/ajusteS.py
+++ proyectoFinal/ajusteS.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy.optimize import minimize
 
 #Tamaño del sistema.
-Lx=3
-Ly=3
+Lx=2
+Ly=50
 num_cells = Lx*Ly
 
 #Función que hace el papel de la simulación de LatticeBoltzmann
@@ -29,34 +29,25 @@ def objective(params, density_observed):
 def optimize_parameters_for_cells(density_observed):
 
     S_initial = np.full(num_cells, 1.0)   #Valores iniciales para S
-
     bounds = [(1.0, 10.0)] * num_cells    #Restricciones sobre S
-
     # Optimización usando el método L-BFGS-B (que soporta restricciones de parámetros)
     result = minimize(objective, S_initial, args=(density_observed), method='L-BFGS-B', bounds=bounds)
-
     # Extraer los valores optimizados de S
     S_opt = result.x[:num_cells]
-
     return S_opt
-
-
 
 rhoSize = Lx*Ly;
 rhoObsVector = np.zeros(rhoSize)
-
-
 Sreal= np.random.uniform(1.0, 10.0, size=rhoSize) #Se crea un arreglo de S, que serán los valores reales.
-
 #Se construye un vector de densidades observadas.
 for i in range(0,rhoSize):
     rhoObsVector[i]= Sreal[i]*(i+1)
-
 S_optimized = optimize_parameters_for_cells(rhoObsVector) #Se llama a la función de optimización.
-
 rhoAdjusted = np.zeros(rhoSize)
 for i in range(0,rhoSize):
     rhoAdjusted[i] = S_optimized[i]*(i+1)
+
+
 
 print("\nValores reales:\n")
 print("Valores de S reales:\n",Sreal)
@@ -65,3 +56,4 @@ print("---------------------------------------------\n")
 print("Valores ajustados:\n")
 print("Valores optimizados de S:\n", S_optimized)
 print("rho_Adjusted:\n",rhoAdjusted)
+
